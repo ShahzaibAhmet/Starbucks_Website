b0c49174c12eac2fabbf96cefcc7b77e4b7c4c4e
ShahzaibAhmet <shahzaib.ahmet@gmail.com>
2024-02-27 03:04:12 +0500

    Initial commit

---
+++ star.js
@@ -0,0 +1,13 @@
+function imageslider(anything) {
+    document.querySelector(".Starbucks").src = anything;
+}
+
+function changecolor(color){
+    const circle = document.querySelector('#back');
+    circle.style.background = color ; 
+
+}
+// function fadeInElement() {
+//     const element = document.querySelector('#idk');
+//     element.classList.add('#idk');
+// }
